7a4064c4d7e2329068a44b44ea709a0f401587ad
Nate Moore <nate272@gmail.com>
2023-07-18 12:16:12 -0700

    Update to use weatherapi instead of openweathermap

--- app/weather.js
+++ app/weather.js
@@ -8,6 +8,59 @@ const weatherLabel = document.getElementById('weatherLabel');
 // weatherIcon_800 is our default icon that starts as shown and not hidden
 let currentWeatherIconId = 'weatherIcon_800';
 
+// see https://www.weatherapi.com/docs/weather_conditions.json for a list of weather codes
+const codeToIcon = {
+  1000: "clear",
+  1003: "cloudy",
+  1006: "cloudy",
+  1009: "cloudy",
+  1030: "mist",
+  1063: "rain",
+  1066: "snow",
+  1069: "snow",
+  1072: "drizzle",
+  1087: "thunderstorm",
+  1114: "snow",
+  1117: "snow",
+  1135: "atmosphere",
+  1147: "atmosphere",
+  1150: "drizzle",
+  1153: "drizzle",
+  1168: "drizzle",
+  1171: "drizzle",
+  1180: "rain",
+  1183: "rain",
+  1186: "rain",
+  1189: "rain",
+  1192: "rain",
+  1195: "rain",
+  1198: "rain",
+  1201: "rain",
+  1204: "storm",
+  1207: "storm",
+  1210: "snow",
+  1213: "snow",
+  1216: "snow",
+  1219: "snow",
+  1222: "snow",
+  1225: "snow",
+  1237: "snow",
+  1240: "rain",
+  1243: "rain",
+  1246: "rain",
+  1249: "storm",
+  1252: "storm",
+  1255: "snow",
+  1258: "snow",
+  1261: "snow",
+  1264: "snow",
+  1273: "thunderstorm",
+  1276: "thunderstorm",
+  1279: "thunderstorm",
+  1282: "thunderstorm"
+};
+
+
 // Request weather data from the companion
 // This is set in a timeout in settings.js when the zip code setting is read
 export const requestWeatherFromCompanion = (zipCode, countryCode, units) => {
@@ -22,14 +75,8 @@ export const requestWeatherFromCompanion = (zipCode, countryCode, units) => {
 };
 
 const getIconIdFromWeatherId = weatherId => {
-  // see https://openweathermap.org/weather-conditions for a list of weather codes
-  if (weatherId === 800) {
-    // 800 is a special code for "clear"; everything else is just in a group
-    return 'weatherIcon_800';
-  }
-
-  const weatherIdGroup = `${weatherId}`.substring(0, 1);
-  return `weatherIcon_${weatherIdGroup}xx`;
+  const iconName = codeToIcon[weatherId] || "clear";
+  return `weatherIcon_${iconName}`;
 };
 
 export const onWeatherFetchSuccess = data => {
--- companion/weather.js
+++ companion/weather.js
@@ -1,10 +1,9 @@
 import { WEATHER_FETCH_SUCCESS } from '../common/commands';
-import { getOpenWeatherMapApiKey } from '../common/secrets';
+import { getWeatherApiKey } from '../common/secrets';
 
-const getOpenWeatherMapUrl = (zipCode, countryCode, units) =>
-  `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode ||
-    98102},${countryCode || 'us'}&units=${units ||
-    'imperial'}&appId=${getOpenWeatherMapApiKey()}`;
+const getWeatherApiUrl = (zipCode) =>
+  `http://api.weatherapi.com/v1/current.json?q=${zipCode ||
+    98102}&key=${getWeatherApiKey()}`;
 
 export const queryOpenWeatherMap = async (
   callback,
@@ -12,19 +11,19 @@ export const queryOpenWeatherMap = async (
 ) => {
   try {
     const result = await fetch(
-      getOpenWeatherMapUrl(zipCode, countryCode, units)
+      getWeatherApiUrl(zipCode, countryCode, units)
     );
 
     if (!result.ok) {
       throw new Error(
-        `Non-OK response from openweathermap of ${result.status}`
+        `Non-OK response from weatherapi of ${result.status}`
       );
     }
 
     const data = await result.json();
     callback({
       command: WEATHER_FETCH_SUCCESS,
-      temperature: data.main.temp,
+      temperature: units === "imperial" ? data.current.temp_f : data.current.temp_c,
       weather: data.weather[0].id
     });
   } catch (err) {
